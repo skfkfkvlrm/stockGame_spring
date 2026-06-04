bbe9726e9ea2e332cb43abc4193ac078901e759c
천사무엘 <skmfmfvlrm@gmail.com>
2026-06-04 10:15:27 +0900

    setup

---
+++ src/main/java/com/skfkfkvlrm/stockgame_spring/StockGameSpringApplication.java
@@ -0,0 +1,13 @@
+package com.skfkfkvlrm.stockgame_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockGameSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockGameSpringApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/skfkfkvlrm/stockgame_spring/vo/CouponPurchase.java
@@ -0,0 +1,4 @@
+package com.skfkfkvlrm.stockgame_spring.vo;
+
+public class CouponPurchase {
+}
